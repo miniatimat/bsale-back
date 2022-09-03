7f5ba6eb8ee27e458897e4d8051df0c84596a176
Mateo <miniatimat@gmail.com>
2022-09-03 19:25:30 -0300

    first commit

--- api/index.js
+++ api/index.js
@@ -5,6 +5,5 @@ const { conn } = require("./src/db.js");
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, async () => {
-    console.log("Listening on port 3001"); // eslint-disable-line no-console
   });
 });
